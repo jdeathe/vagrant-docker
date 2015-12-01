8d548bb7ebf4ced75c25bb4cbbacc45e5cf649c3
James Deathe <james.deathe@jadu.net>
2015-12-01 04:53:17 +0000

    Share HOME directory by default instead of specific directories. Don't port forward docker to localhost by default.

--- docker-host/config.rb
+++ docker-host/config.rb
@@ -66,12 +66,12 @@ $enable_serial_logging=false
 # If 2375 is used, Vagrant will auto-increment (e.g. in the case of $num_instances > 1)
 # You can then use the docker tool locally by setting the following env var:
 #   export DOCKER_HOST='tcp://127.0.0.1:2375'
-$expose_docker_tcp=2375
+#$expose_docker_tcp=2375
 
 # Enable NFS sharing of your home directory ($HOME) to CoreOS
 # It will be mounted at the same path in the VM as on the host.
 # Example: /Users/foobar -> /Users/foobar
-$share_home=false
+$share_home=true
 
 # Customize VMs
 $vm_gui = false
@@ -83,7 +83,7 @@ $vm_cpus = 1
 # $shared_folders = {'/path/on/host' => '/path/on/guest', '/home/foo/app' => '/app'}
 # or, to map host folders to guest folders of the same name,
 # $shared_folders = Hash[*['/home/foo/app1', '/home/foo/app2'].map{|d| [d, d]}.flatten]
-$shared_folders = {'./config' => '/etc/services-config', './data' => '/var/services-data', './packages' => '/var/services-packages'}
+#$shared_folders = {'./config' => '/etc/services-config', './data' => '/var/services-data', './packages' => '/var/services-packages'}
 
 # Enable port forwarding from guest(s) to host machine, syntax is: { 80 => 8080 }, auto correction is enabled by default.
 #$forwarded_ports = {}
