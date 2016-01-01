a6534eefc27b176c399de1001bb972ff31f16678
James Deathe <james.deathe@jadu.co.uk>
2016-01-01 23:46:15 +0000

    Add example mappings.

--- core-docker/config.rb
+++ core-docker/config.rb
@@ -85,6 +85,7 @@ $vm_cpus = 1
 # $shared_folders = {'/path/on/host' => '/path/on/guest', '/home/foo/app' => '/app'}
 # or, to map host folders to guest folders of the same name,
 # $shared_folders = Hash[*['/home/foo/app1', '/home/foo/app2'].map{|d| [d, d]}.flatten]
+#$shared_folders = {'~/services-config' => '/etc/services-config', '~/services-data' => '/var/services-data', '~/services-packages' => '/var/services-packages'}
 #$shared_folders = {'./config' => '/etc/services-config', './data' => '/var/services-data', './packages' => '/var/services-packages'}
 $shared_folders = {'./config' => '/etc/services-config'}
 
